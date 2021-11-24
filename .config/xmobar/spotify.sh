28dc4181e67f29c3bc471da92322f0195d6c68b2
Rice <ricebalvids@gmail.com>
2021-11-24 03:03:28 +0100

    added ranger options and modified spotify script

--- .config/xmobar/spotify.sh
+++ .config/xmobar/spotify.sh
@@ -1,7 +1,7 @@
 
 spotifyid=$(ps -ef | grep -e '[/]usr/share/spotify' -e '[/]opt/spotify' | awk '{print $2}' | head -1)
 if [ "$spotifyid" != "" ]; then
-    artist=$(playerctl metadata artist)
-    song=$(playerctl metadata title | cut -c 1-60)
+    artist=$(playerctl -p spotify metadata artist)
+    song=$(playerctl -p spotify metadata title | cut -c 1-60)
     echo -n "$artist · $song"
 fi
