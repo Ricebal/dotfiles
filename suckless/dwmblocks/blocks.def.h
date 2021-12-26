a79a8f67d316bafbc6a2143b11947b2244aee835
Rice <ricebalvids@gmail.com>
2021-12-26 20:13:06 +0100

    dwmblocks update

--- suckless/dwmblocks/blocks.def.h
+++ suckless/dwmblocks/blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "/home/rice/suckless/dwmblocks/scripts/spotify.sh", 					5, 		0},
+	{"", "/home/rice/suckless/dwmblocks/scripts/spotify.sh", 					10, 		0},
 	{"💻: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	{"", "/home/rice/suckless/dwmblocks/scripts/date.sh",					5,		0},
 };
